60092c696c8e5c591d7b29609a26811a7f3db68d
yollct <chit-tong.lio@wzw.tum.de>
2022-10-18 15:06:54 +0200

    docs

--- spycone/__init__.py
+++ spycone/__init__.py
@@ -4,7 +4,7 @@ from .clustering import clustering
 from .iso import iso_function
 from .preprocess import preprocess
 from .visualize import vis_all_clusters,  switch_plot, gsea_plot, vis_modules, vis_better_modules, compare_mod
-from .go_terms import clusters_gsea, modules_gsea, list_genesets, list_gsea
+from .go_terms import clusters_gsea, modules_gsea, list_gsea
 #from .connectivity import connectivity
 from .run_domino import run_domino, run_domain_domino
 from .DOMINO.src.core import domino
